af2439d1dc0d7e1dced1082e4b1f5fd612b55397
Igor Caciatori <caciatori07@gmail.com>
2025-02-11 19:58:53 -0300

    Initial commit

---
+++ src/test/java/com/todolist/caciatori/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.caciatori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
